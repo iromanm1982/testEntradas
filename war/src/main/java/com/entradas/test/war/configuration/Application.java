a07931ec134bc81dbf48ceb18e40272930b830a0
Muerte <ivan.roman.muerte@gmail.com>
2017-10-28 12:45:08 +0200

    spring boot configuration

---
+++ war/src/main/java/com/entradas/test/war/configuration/Application.java
@@ -0,0 +1,27 @@
+package com.entradas.test.war.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+@Configuration
+@PropertySource("file:/var/entradas/test/properties/desa/enviroment.yaml")
+@EnableAutoConfiguration
+@ComponentScan("com.entradas.test")
+public class Application extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
